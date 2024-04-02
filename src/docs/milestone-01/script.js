72a9108d28dc0ba90f7a462c15c91a4087d5e6d3
chaitanya12350 <csachdeva@umass.edu>
2024-04-02 18:19:07 -0400

    i

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navItems = document.querySelectorAll('.nav-item');
-  
-    navItems.forEach(item => {
-      item.addEventListener('click', function() {
-        navItems.forEach(nav => nav.classList.remove('active'));
-        this.classList.add('active');
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const navItems = document.querySelectorAll(".nav-item");
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      navItems.forEach((nav) => nav.classList.remove("active"));
+      this.classList.add("active");
     });
   });
-  
+});
