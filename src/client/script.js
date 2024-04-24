f354d30f91661079664383ef105f3acd0c13c415
chaitanya12350 <csachdeva@umass.edu>
2024-04-24 17:03:22 -0400

    filter
    sorted filter

--- src/client/script.js
+++ src/client/script.js
@@ -1,142 +1,144 @@
-
 var db = new PouchDB('mydb')
 db.info().then((info)=>{
     console.log(info)
 })
 
 const classData = [
-    {
-      _id: '1',
-      class_name: 'Mixed Martial Arts',
-      category: 'MMA',
-      category_id: 'MA',
-      day_1: 'Monday',
-      day_2: 'Wednesday',
-      instructor_name: 'Maria',
-      total_seats: 50,
-      seats_booked: 0,
-      seats_available: 50
-    },
-    {
-      _id: '2',
-      class_name: 'Boxing',
-      category: 'MMA',
-      category_id: 'MA',
-      day_1: 'Wednesday',
-      day_2: 'Friday',
-      instructor_name: 'Partick',
-      total_seats: 30,
-      seats_booked: 0,
-      seats_available: 30
-    },
-    {
-      _id: '3',
-      class_name: 'KickBoxing',
-      category: 'MMA',
-      category_id: 'MA',
-      day_1: 'Monday',
-      day_2: 'Wednesday',
-      instructor_name: 'Jason',
-      total_seats: 28,
-      seats_booked: 0,
-      seats_available: 28
-    },
-    {
-      _id: '4',
-      class_name: 'Yoga',
-      category: 'Wellness',
-      category_id: 'W',
-      day_1: 'Tuesday',
-      day_2: 'Thursday',
-      instructor_name: 'Jack',
-      total_seats: 45,
-      seats_booked: 0,
-      seats_available: 45
-    },
-    {
-      _id: '5',
-      class_name: 'Meditation',
-      category: 'Wellness',
-      category_id: 'W',
-      day_1: 'Wednesday',
-      day_2: 'Thursday',
-      instructor_name: 'raphael',
-      total_seats: 30,
-      seats_booked: 0,
-      seats_available: 30
-    },
-    {
-      _id: '6',
-      class_name: 'HIIT',
-      category: 'Cardio',
-      category_id: 'Car',
-      day_1: 'Thursday',
-      day_2: 'Friday',
-      instructor_name: 'Wilson',
-      total_seats: 55,
-      seats_booked: 0,
-      seats_available: 55
-    },
-    {
-      _id: '7',
-      class_name: 'LIIT',
-      category: 'Cardio',
-      category_id: 'Car',
-      day_1: 'Monday',
-      day_2: 'Wednesday',
-      instructor_name: 'Derick',
-      total_seats: 20,
-      seats_booked: 0,
-      seats_available: 20
-    },
-    {
-      _id: '8',
-      class_name: 'Indoor cycling',
-      category: 'Cardio',
-      category_id: 'Car',
-      day_1: 'Wednesday',
-      day_2: 'Friday',
-      instructor_name: 'Dave',
-      total_seats: 55,
-      seats_booked: 0,
-      seats_available: 55
-    },
-    {
-      _id: '9',
-      class_name: 'Abs',
-      category: 'Muscle Building',
-      category_id: 'MB',
-      day_1: 'Monday',
-      day_2: 'Wednesday',
-      instructor_name: 'Larissa',
-      total_seats: 42,
-      seats_booked: 0,
-      seats_available: 42
-    },
-    {
-      _id: '10',
-      class_name: 'Upper Body',
-      category: 'Muscle Building',
-      category_id: 'MB',
-      day_1: 'Tuesday',
-      day_2: 'Thursday',
-      instructor_name: 'Maria',
-      total_seats: 35,
-      seats_booked: 0,
-      seats_available: 35
-    },
-    {
-      _id: '11',
-      class_name: 'Lowe Body',
-      category: 'Muscle Building',
-      category_id: 'MB',
-      day_1: 'Thursday',
-      day_2: 'Friday',
-      instructor_name: 'Alphonso',
-      total_seats: 50,
-      seats_booked: 0,
-      seats_available: 50
-    }
+    
+        {
+          "_id": "1",
+          "class_name": "Mixed Martial Arts",
+          "category": "MMA",
+          "category_id": "MA",
+          "days": ["Monday", "Wednesday"],
+          "instructor_name": "Maria",
+          "total_seats": 50,
+          "seats_booked": 0,
+          "seats_available": 50,
+          "img": "mma.jpg"
+        },
+        {
+          "_id": "2",
+          "class_name": "Boxing",
+          "category": "MMA",
+          "category_id": "MA",
+          "days": ["Wednesday", "Friday"],
+          "instructor_name": "Partick",
+          "total_seats": 30,
+          "seats_booked": 0,
+          "seats_available": 30,
+          "img": "mma.jpg"
+        },
+        {
+          "_id": "3",
+          "class_name": "KickBoxing",
+          "category": "MMA",
+          "category_id": "MA",
+          "days": ["Monday", "Wednesday"],
+          "instructor_name": "Jason",
+          "total_seats": 28,
+          "seats_booked": 0,
+          "seats_available": 28,
+          "img": "mma.jpg"
+        },
+        {
+          "_id": "4",
+          "class_name": "Yoga",
+          "category": "Wellness",
+          "category_id": "W",
+          "days": ["Tuesday", "Thursday"],
+          "instructor_name": "Jack",
+          "total_seats": 45,
+          "seats_booked": 0,
+          "seats_available": 45,
+          "img": "wellness.jpg"
+        },
+        {
+          "_id": "5",
+          "class_name": "Meditation",
+          "category": "Wellness",
+          "category_id": "W",
+          "days": ["Wednesday", "Thursday"],
+          "instructor_name": "Raphael",
+          "total_seats": 30,
+          "seats_booked": 0,
+          "seats_available": 30,
+          "img": "wellness.jpg"
+        },
+        {
+          "_id": "6",
+          "class_name": "HIIT",
+          "category": "Cardio",
+          "category_id": "Car",
+          "days": ["Thursday", "Friday"],
+          "instructor_name": "Wilson",
+          "total_seats": 55,
+          "seats_booked": 0,
+          "seats_available": 55,
+          "img": "cardio.jpg"
+        },
+        {
+          "_id": "7",
+          "class_name": "LIIT",
+          "category": "Cardio",
+          "category_id": "Car",
+          "days": ["Monday", "Wednesday"],
+          "instructor_name": "Derick",
+          "total_seats": 20,
+          "seats_booked": 0,
+          "seats_available": 20,
+          "img": "cardio.jpg"
+        },
+        {
+          "_id": "8",
+          "class_name": "Indoor cycling",
+          "category": "Cardio",
+          "category_id": "Car",
+          "days": ["Wednesday", "Friday"],
+          "instructor_name": "Dave",
+          "total_seats": 55,
+          "seats_booked": 0,
+          "seats_available": 55,
+          "img": "cardio.jpg"
+        },
+        {
+          "_id": "9",
+          "class_name": "Abs",
+          "category": "Muscle Building",
+          "category_id": "MB",
+          "days": ["Monday", "Wednesday"],
+          "instructor_name": "Larissa",
+          "total_seats": 42,
+          "seats_booked": 0,
+          "seats_available": 42,
+          "img": "building.jpg"
+        },
+        {
+          "_id": "10",
+          "class_name": "Upper Body",
+          "category": "Muscle Building",
+          "category_id": "MB",
+          "days": ["Tuesday", "Thursday"],
+          "instructor_name": "Maria",
+          "total_seats": 35,
+          "seats_booked": 0,
+          "seats_available": 35,
+          "img": "building.jpg"
+        },
+        {
+          "_id": "11",
+          "class_name": "Lower Body",
+          "category": "Muscle Building",
+          "category_id": "MB",
+          "days": ["Thursday", "Friday"],
+          "instructor_name": "Alphonso",
+          "total_seats": 50,
+          "seats_booked": 0,
+          "seats_available": 50,
+          "img": "building.jpg"
+        }
+    
+    
   ];
   db.bulkDocs(classData,function(err,res){
     if(err){
@@ -145,6 +147,7 @@ const classData = [
     else{
         console.log("data created")
     }});
+
 
 
 
@@ -173,7 +176,7 @@ const classData = [
 //         console.error('Error fetching products by category:', err);
 //     }
 // } 
-async function getProductsByCategory(category) {
+async function getProductsByCategory(category,days) {
     try {
         // Fetch all documents, including their details
         const result = await db.allDocs({
@@ -181,8 +184,11 @@ async function getProductsByCategory(category) {
         });
 
         // Use JavaScript to filter documents by category
-        const filteredProducts = result.rows.filter(row => row.doc.category && row.doc.category === category).map(row => row.doc);
-
+        //const filteredProducts = result.rows.filter(row => row.doc.category && row.doc.category === category).map(row => row.doc);
+        const filteredProducts = result.rows.filter(row => {
+            return row.doc.category && row.doc.category === category &&
+                   row.doc.days && row.doc.days.some(day => row.doc.days.includes(days));
+        }).map(row => row.doc);
         return filteredProducts;
     } catch (err) {
         console.error('Error fetching products by category:', err);
@@ -197,10 +203,11 @@ async function updateProductDisplay(products) {
         var productData =product
         var productDiv = document.createElement('div'); 
         console.log(productData);
+        console.log(productData.img)
         productDiv.className = 'product';
         productDiv.innerHTML = `
-            <div class="class-card " data-category="zumba">
-            
+            <div class="class-card " data-category="${productData.class_name}">
+            <img class="class-image" src="${productData.img}" alt="${productData.class_name}">
             <h3 class="class-title">${productData.class_name}</h3>
         </div>
         `
@@ -239,10 +246,15 @@ async function updateProductDisplay(products) {
     document.addEventListener('DOMContentLoaded',  function() {
         var db = new PouchDB('mydb'); // Corrected database name for consistency
         var categorySelect = document.getElementById('category');
-    
-        categorySelect.addEventListener('change', function() {
-            displayProducts(this.value); // Pass the selected value
+        var days = document.getElementById('day')
+        var searchButton = document.querySelector('button[type="submit"]')
+        searchButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default form submit action
+            displayProducts(categorySelect.value,days.value); // Pass the selected value from the select element
         });
+        // categorySelect.addEventListener(var'change', function() {
+        //     displayProducts(this.value); // Pass the selected value
+        // });
     
         // Function to fetch and display products based on category
         //  function displayProducts(category) {
@@ -256,13 +268,13 @@ async function updateProductDisplay(products) {
         //         }
         //     });
         // } 
-        function displayProducts(category) {
+        function displayProducts(category,days) {
             db.allDocs({ include_docs: true, descending: true }, function(err, doc) {
                 if (err) {
                     console.error(err);
                 } else {
                     // Since getProductsByCategory returns a promise, you need to wait for it to resolve
-                    getProductsByCategory(category).then(function(filteredDocs) {
+                    getProductsByCategory(category,days).then(function(filteredDocs) {
                         // Now filteredDocs should be the array you expect
                         console.log(filteredDocs); // This should log the array to the console
                         updateProductDisplay(filteredDocs); // Call the function with the array
